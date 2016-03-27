64cf1fde4e364e2f07b1c3b0a75cb2089f72cd83
doug <dougbay@gmail.com>
2016-03-27 11:33:37 -0600

    first commit

---
+++ src/main/java/testClass.java
@@ -0,0 +1,39 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by doug on 3/27/2016.
+ */
+public class testClass {
+    public static void main(String[] args) throws IOException {
+        File file= new File("C:\\Users\\doug\\Desktop\\dev\\png\\image-189.png");
+        BufferedImage image = ImageIO.read(file);
+        // Getting pixel color by position x and y
+        int totalheight = image.getHeight();
+        int totalwidth = image.getWidth();
+        int pixelnumber = 0;
+        int  red = 0;
+        int  green = 0;
+        int  blue = 0;
+        int clr = 0;
+        String friendly;
+        for (int w = 0; w < image.getWidth(); w++)
+        {
+            for (int h = 0; h < image.getHeight(); h++) {
+                friendly = "h"+Integer.toString(h)+"w"+Integer.toString(w);
+                clr =  image.getRGB(w,h);
+                red   = (clr & 0x00ff0000) >> 16;
+                green = (clr & 0x0000ff00) >> 8;
+                blue  =  clr & 0x000000ff;
+
+            }
+        }
+
+        int lastRed = red;
+        int lastGreen = green;
+        int lastBlue = blue;
+        String tmp = "tmp";
+    }
+}
